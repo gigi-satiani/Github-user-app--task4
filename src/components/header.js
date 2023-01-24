4fc9c6c483b18245467e6bf57146f10b9d15bc56
Gigi Asatiani <123gigiasatiani123@gmail.com>
2023-01-24 17:48:00 +0400

    finish

---
+++ src/components/header.js
@@ -0,0 +1,62 @@
+import "../App.css";
+import mySun from "./images/sun.svg";
+import myMoon from "./images/moon.svg";
+import mySearch from "./images/search.svg";
+import React, { useEffect, useState } from "react";
+import { StyledButton, ErrorMessage } from "./themeMode.style";
+
+const Header = (props) => {
+  const toggleTheme = () => {
+    props.setTheme(props.theme === "light" ? "dark" : "light");
+  };
+  
+
+  return (
+    <div className="headerParent">
+      <div className="darkLightMode">
+        <span className="devfinderSpan">devfinder</span>
+        <StyledButton
+          onClick={() => {
+            toggleTheme();
+          }}
+        >
+          <div style={{ display: props.theme === "light" ? "block" : "none" }}>
+            LIGHT
+          </div>
+          <div style={{ display: props.theme === "dark" ? "block" : "none" }}>
+            DARK
+          </div>
+          <img
+            src={mySun}
+            className="buttonImage"
+            style={{ display: props.theme === "light" ? "block" : "none" }}
+          />
+          <img
+            src={myMoon}
+            className="buttonImage"
+            style={{ display: props.theme === "dark" ? "block" : "none" }}
+          />
+        </StyledButton>
+      </div>
+      <div className="searchParent">
+        <div className="searchInputParent">
+          <figure className="figureParent">
+            <img src={mySearch} className="searchIcon" />
+          </figure>
+          <input
+            type="search"
+            className="searchInput"
+            placeholder="Search GitHub username..."
+            style={{ color: props.theme === "light" ? "#fff" : "#4B6A9B" }}/>
+            <ErrorMessage style={{color: '#F74646', fontFamily: "monospace", fontSize: '15px',
+             paddingRight: '5px', display: 'flex', alignItems: 'center'}}>{props.data.message && 'No results'}</ErrorMessage>
+        </div>
+        <div className="submitParent">
+          <button onClick={props.findUser}>Search</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
