3b90d34f5f039208f6b7b5bc5dbc75c24f7cfb9b
Gigi Asatiani <123gigiasatiani123@gmail.com>
2023-01-24 17:49:37 +0400

    gituser-finish

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./components/header";
+import Main from "./components/mainBody";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./components/themeMode.style";
+
+export const lightTheme = {
+  body: "#F2F2F2",
+  fontColor: "#2B3442",
+  cardColor: "#FEFEFE",
+  followersDiv: "#F6F8FF",
+};
+export const darkTheme = {
+  body: "#2B3442",
+  fontColor: "#ffffff",
+  cardColor: "#1E2A47",
+  followersDiv: "#141D2F",
+};
 
 function App() {
+  const [theme, setTheme] = useState("light");
+  const [data, setData] = useState('');
+
+  function findUser() {
+    const findUserName = document.querySelector(".searchInput").value;
+    fetch(`https://api.github.com/users/${findUserName}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data, 'dataa')
+        return setData(data);
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme === "light" ? darkTheme : lightTheme}>
+      <GlobalStyle />
+      <div className="App">
+        <div className="bigDaddyDiv">
+          <Header theme={theme} setTheme={setTheme} findUser={findUser} data={data} />
+          <Main theme={theme} setTheme={setTheme} data={data} />
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/themeMode.style.js
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
+
+export const StyledButton = styled.button`
+  background-color: ${props => props.theme.body};
+  color: ${props => props.theme.fontColor};
+  width: 100px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  justify-content: space-around;
+  cursor: pointer;
+  letter-spacing: 2px;
+  font-size: 14px;
+  font-weight: 600;
+  border: none;
+
+  &:hover {
+    color: gray;
+  }
+`
+export const ErrorMessage = styled.span `
+.inputErrorMsg {
+  width: 100%;
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  font-weight: 600;
+  color: #F74646 !important;
+  font-family: monospace;
+}
+`
+export const GlobalStyle = createGlobalStyle`
+.App {
+  background-color: ${props => props.theme.body};
+}
+.searchParent {
+  background-color: ${props => props.theme.cardColor};
+}
+span, .oqtoDiv h2, .darkLightMode span, p, .joinDate, .userBio span,
+.searchInput::placeholder {
+  color: ${props => props.theme.fontColor};
+}
+.mainParent {
+  background-color: ${props => props.theme.cardColor};
+}
+.flexContainer {
+  background-color: ${props => props.theme.followersDiv};
+}
+`
